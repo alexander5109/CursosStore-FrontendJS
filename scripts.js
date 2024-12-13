7982015a3136213f91a4ec4af719bb012ce849b1
alex5109 <xanderseling@gmail.com>
2024-12-12 22:17:20 -0300

    Agregue el fetch de la api (y nada mas) Ahi hice andar la api de productos y con filtro de lo mas parecido a la informatica que encontre. No son cursos pero bueno.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,51 @@
-console.log("Hola Mundo");
+document.addEventListener("DOMContentLoaded", () => {
+	let contenedor_de_cards = document.getElementById("contenedor_de_cards");
+
+	function fetchProductos() {
+		let limite = 0;
+		const productos_que_me_interesan = ["laptops", "mobile-accessories", "smartphones", "tablets"];
+		
+		fetch(`https://dummyjson.com/products?limit=0`)
+			.then((response) => response.json())
+			.then((data) => {
+				const productos = data.products;
+
+				contenedor_de_cards.innerHTML = ""; // Limpiea
+
+				for (let i = 0; i < productos.length; i++) {
+					let producto = productos[i];
+					if (!productos_que_me_interesan.includes(producto.category)) {
+						continue;
+					} 
+					if (limite >= 8) {
+						break; // Basta
+					}
+					
+					// Crear las cards
+					const cardCol = document.createElement("div");
+					cardCol.className = "col";
+
+					cardCol.innerHTML = `
+						<div class="card">
+							<img src="${producto.thumbnail}" class="card-img-top" alt="${producto.title}">
+							<div class="card-body">
+								<h5 class="card-title">${producto.title}</h5>
+								<p class="card-text">${producto.description}</p>
+								<div class="btn-group-toggle" data-toggle="buttons">
+									<label class="btn btn-secondary active">
+										<input type="checkbox" checked autocomplete="off"> Agregado al chango
+									</label>
+								</div>
+							</div>
+						</div>
+					`;
+
+					limite++; // Increment the product count
+					contenedor_de_cards.appendChild(cardCol);
+				}
+			})
+			.catch((error) => console.error("Error fetching products:", error));
+	}
+
+	fetchProductos();
+});
